2e70586d5e87863e64e1623b066846294db05616
vladprosolupov <prosolupov97@gmail.com>
2018-11-24 12:43:58 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-multiple-empty-lines': ['error', { 'max': 2 }],
+    'vue/html-closing-bracket-spacing': 0,
+    'vue/max-attributes-per-line': [2, {
+      'singleline': 3,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }]
+  }
+}
